276a8673b5e3bbd776061022aa5e250481b0a7b8
Fagu12 <102469304+Fagu12@users.noreply.github.com>
2022-09-24 20:16:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ module.exports = {
 	HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
 	HEROKU_API_KEY: process.env.HEROKU_API_KEY,
 	BRANCH: 'master',
-	STICKER_PACKNAME: process.env.STICKER_PACKNAME || '❤️,LyFE',
+	STICKER_PACKNAME: process.env.STICKER_PACKNAME || '𝐹𝛥𝐺𝑈 𝐵𝛩𝑇⚡',
 	ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
 	LOG_MSG: toBool(process.env.LOG_MSG) || false,
 	RMBG_KEY: process.env.RMBG_KEY || 'null',
